d29fed09b69b5001eec2c4690bef10e7200af08d
Paweł Wasilewski <pwasilewski-netcode@users.noreply.github.com>
2018-04-20 21:04:18 +0200

    MOD Poprawienie pliku webpack.config.js dla js-lab

--- js-lab/webpack.config.js
+++ js-lab/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
 	entry: ['./js/app.js', './js/ex2.js', './js/ex3.js'],
 	output: {
 		path: path.resolve(__dirname, 'js'), //output directory
-		filename: 'out.js' //output file (merge all JS-files will into one out.js file)
+		filename: 'out.js', //output file (merge all JS-files will into one out.js file)
+		publicPath: 'js'
 	},
 	module: {
 		rules: [
